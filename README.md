186f6a4a1ede4b9f3bd0e56d187cc7aa4f7fcb56
Bikrant Singh <Bikrant.Singh@XEBIAINDIA.LOCAL>
2020-05-29 19:16:37 +0530

    Create xebia assignment app

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-# xebia-react-assignment
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ build/precache-manifest.bdf8b946d7302b2283efd39f7d3d0d60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c028f0192212d2628b565d7b7ca2eed1",
+    "url": "/xebia-react-assignment/index.html"
+  },
+  {
+    "revision": "a311c92e13f4635001c6",
+    "url": "/xebia-react-assignment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba4afbe67bbcd708fdc6",
+    "url": "/xebia-react-assignment/static/js/2.cf891c47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xebia-react-assignment/static/js/2.cf891c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a311c92e13f4635001c6",
+    "url": "/xebia-react-assignment/static/js/main.a0b951f1.chunk.js"
+  },
+  {
+    "revision": "632ddcdb4b65d5a42b10",
+    "url": "/xebia-react-assignment/static/js/runtime-main.c2670432.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xebia-react-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
