7fd07e673859f31e20bb8da5efcb3ca4aa5db1f4
Bikrant Singh <Bikrant.Singh@XEBIAINDIA.LOCAL>
2020-05-30 16:25:05 +0530

    build

---
+++ build/precache-manifest.9af3944060a60c9510b64a54de2b93de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908a3b84504438e09d3f2145e22dd8cf",
+    "url": "/xebia-react-assignment/index.html"
+  },
+  {
+    "revision": "a9a126b21c7cd9c84441",
+    "url": "/xebia-react-assignment/static/css/main.7cbeb96d.chunk.css"
+  },
+  {
+    "revision": "aaf4c741ec90b098f007",
+    "url": "/xebia-react-assignment/static/js/2.d2787be5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xebia-react-assignment/static/js/2.d2787be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a126b21c7cd9c84441",
+    "url": "/xebia-react-assignment/static/js/main.b27b4e7f.chunk.js"
+  },
+  {
+    "revision": "632ddcdb4b65d5a42b10",
+    "url": "/xebia-react-assignment/static/js/runtime-main.c2670432.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ea60cea6a4f6681ca45c07d669f4f08.js"
+  "/xebia-react-assignment/precache-manifest.9af3944060a60c9510b64a54de2b93de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xebia-react-assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
