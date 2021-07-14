fa5ec0fbe8e72e1a25b1fc133f3381370b8d35d3
Bikrant Singh <Bikrant.Singh@XEBIAINDIA.LOCAL>
2021-07-14 17:00:57 +0530

    Change API url

--- src/config/ServiceUrl.js
+++ src/config/ServiceUrl.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://xebiascart.herokuapp.com';
+export const BASE_URL = 'https://xebia-scart.herokuapp.com';
 export const LOGIN_URL = BASE_URL + '/users';
 export const PRODUCTS_URL = BASE_URL + '/products';
 export const FILTERS_URL = BASE_URL + '/filters';
